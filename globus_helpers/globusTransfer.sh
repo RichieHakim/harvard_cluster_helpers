e29a2e3dd9bd787604a53a4b4a7f21352d22c978
RichieHakim <RichHakim@gmail.com>
2024-03-12 01:13:52 -0400

    Refactor globusTransfer.sh script to improve readability and add input validation

--- globus_helpers/globusTransfer.sh
+++ globus_helpers/globusTransfer.sh
@@ -1,19 +1,47 @@
-#This is to recursively transfer data from our HMS server to the Harvard FAS RC Holyoke server
-# This script is to be run on HMS O2 and requires that pipx and globus-cli is installed and configured
+#!/bin/bash
 
-#You will need to login to globus first AND find the endpoint IDs for both HMS and Harvard by running the following commands:
-# globus endpoint search 'HMS-RC-Endpoint' and globus endpoint search 'Harvard FAS RC Holyoke'
+## Script for transfer between HMS/O2 <--> Harvard FAS RC Holyoke
+## Uses globus-cli. Install with:
+## python -m pip install pipx
+## pipx install globus-cli
 
-
-#List endpoints 
-ep1=b0718922-7031-11e9-b7f8-0a37f382de32    #HMS - use the second UUID 'HMS-RC' NOT 'HMS-RC-Endpoint'
-ep2=1156ed9e-6984-11ea-af52-0201714f6eab    #Harvard
-
-#You will have access to your directories on O2 and the Kempner cluster
-
-from_dir=/n/data1/hms/neurobio/sabatini/Janet/file_in_dirs
-to_dir=/n/holylabs/LABS/bsabatini_lab/Users/jbw25/
-
-globus transfer $ep1:$from_dir $ep2:$to_dir --recursive --label "HMS to Harvard" 
+## You will need to login to globus first AND find the endpoint IDs for both HMS and Harvard by running the following commands:
+## globus endpoint search 'HMS-RC-Endpoint' and globus endpoint search 'Harvard FAS RC Holyoke'
 
 
+# Check for the correct number of arguments
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 name_from path_from name_to path_to"
+    exit 1
+fi
+
+# Process inputs
+name_from=$1
+path_from=$2
+name_to=$3
+path_to=$4
+
+# List endpoints
+declare -A endpoints=(
+    [hms]=b0718922-7031-11e9-b7f8-0a37f382de32
+    [fas]=1156ed9e-6984-11ea-af52-0201714f6eab
+)
+
+# Map names to endpoints
+ep_from=${endpoints[$name_from]}
+ep_to=${endpoints[$name_to]}
+
+# Check if endpoints were correctly assigned
+if [ -z "$ep_from" ]; then
+    echo "Invalid endpoint_from name: $name_from"
+    exit 1
+elif [ -z "$ep_to" ]; then
+    echo "Invalid endpoint_to name: $name_to"
+    exit 1
+fi
+
+# Perform the Globus transfer
+echo ""
+echo "globus transfer $ep_from:$path_from $ep_to:$path_to --recursive --label '$name_from to $name_to'"
+# Replace echo with actual command call when ready
+globus transfer $ep_from:$path_from $ep_to:$path_to --recursive --label "$name_from to $name_to"
