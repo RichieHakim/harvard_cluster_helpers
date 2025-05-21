5cfe63566e93281a0bd0c22dc6dda6a53fff0c08
Richard Hakim <richhakim@gmail.com>
2025-05-20 20:11:38 -0400

    some updates

--- clear_disk_space.sh
+++ clear_disk_space.sh
@@ -1,4 +1,3 @@
-$HOMEDIR='/n/home03/rhakim'
 # sudo
 
 conda clean --all
@@ -8,10 +7,10 @@ conda clean --all
 # sudo apt purge snapd
 
 ## remove and remake homedir / .cache / pip / http and http-v2
-rm -R $HOMEDIR/.cache/pip/http
-rm -R $HOMEDIR/.cache/pip/http-v2
-mkdir $HOMEDIR/.cache/pip/http
-mkdir $HOMEDIR/.cache/pip/http-v2
+rm -R /n/home03/rhakim/.cache/pip/http
+rm -R /n/home03/rhakim/.cache/pip/http-v2
+mkdir /n/home03/rhakim/.cache/pip/http
+mkdir /n/home03/rhakim/.cache/pip/http-v2
 
 # ## Clear /tmp folder will delete all files and folders older than 10 days.
 # sudo find /tmp -ctime +10 -exec rm -rf {} +
@@ -22,3 +21,6 @@ docker image prune --all
 # conda activate env
 # python -m pip cache purge
 #################################
+
+
+rm -R /n/home03/rhakim/wandb
